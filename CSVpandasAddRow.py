5df2333d363fa9130b85077b17ecf36562eeef72
ferdin1009 <ferdin1009@outlook.com>
2021-04-16 11:34:10 +0200

    Add row

---
+++ CSVpandasAddRow.py
@@ -0,0 +1,9 @@
+import pandas
+
+df = pandas.read_csv('hrdata_modified.csv')
+
+newEntry = ['Eric Edward','2014-06-23',71000.00,5]
+
+df.loc[len(df.index)+1] = newEntry
+
+df-to_csv('hrdata_modified.csv', index=False)
--- CSVpandasWrite.py
+++ CSVpandasWrite.py
@@ -4,4 +4,4 @@ index_col='Employee',
 parse_dates=['Hired'],
 header=0, 
 names=['Employee', 'Hired', 'Salary', 'Sick Days'])
-df.to_csv('hrdata_modified.csv')
+df.to_csv('hrdata_modified.csv')    
