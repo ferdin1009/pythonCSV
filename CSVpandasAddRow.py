f3641d8337d9d65d7fd604ef79090ba5b76aac73
ferdin1009 <ferdin1009@outlook.com>
2021-04-16 11:37:06 +0200

    added new emplyee

--- CSVpandasAddRow.py
+++ CSVpandasAddRow.py
@@ -6,4 +6,4 @@ newEntry = ['Eric Edward','2014-06-23',71000.00,5]
 
 df.loc[len(df.index)+1] = newEntry
 
-df-to_csv('hrdata_modified.csv', index=False)
+df.to_csv('hrdata_modified.csv', index=False)
